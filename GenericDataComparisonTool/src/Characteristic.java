52b007921c66d520b53c602e1b31ce199d334f56
Shaunavon Blackmore <Shaunavon Blackmore@192.168.0.12>
2018-07-16 20:56:01 -0500

    Add getter and setter to Characteristic class

--- GenericDataComparisonTool/src/Characteristic.java
+++ GenericDataComparisonTool/src/Characteristic.java
@@ -9,7 +9,7 @@ public class Characteristic {
 	private double firstQuartile;
 	private double thirdQuartile;
 	private String value;
-	private Object betterValue;
+	private BetterValue betterValue;
 	
 	public Characteristic(String attribute, double minValue, double maxValue, double avgValue, 
 			double medianValue, double scoreWgtValue, double firstQrtile, double thirdQrtile, String value)
@@ -73,5 +73,105 @@ public class Characteristic {
 		}
 		else
 			return BetterValue.LOWEST;
+	}
+	
+	public String getAttribute()
+	{
+		return attribute;
+	}
+	
+	public String getValue()
+	{
+		return this.value;
+	}
+	
+	public double getMinimumValue()
+	{
+		return minimumValue;
+	}
+	
+	public double getMaximumValue()
+	{
+		return maximumValue;
+	}
+	
+	public double getAverageValue()
+	{
+		return averageValue;
+	}
+	
+	public double getMedianValue()
+	{
+		return medianValue;
+	}
+	
+	public double getScoreWeightValue()
+	{
+		return scoreWeightValue;
+	}
+	
+	public double getFirstQuartile()
+	{
+		return firstQuartile;
+	}
+	
+	public double getThirdQuartile()
+	{
+		return thirdQuartile;
+	}
+	
+	public BetterValue getBetterValue()
+	{
+		return betterValue;
+	}
+	
+	public void setAttribute(String attribute)
+	{
+		this.attribute = attribute;
+	}
+	
+	public void setValue(String value)
+	{
+		this.value = value;
+	}
+	
+	public void setMinimumValue(double minValue)
+	{
+		this.minimumValue = minValue;
+	}
+	
+	public void setMaximumValue(double maxValue)
+	{
+		this.maximumValue = maxValue;
+	}
+	
+	public void setAverageValue(double avgValue)
+	{
+		this.averageValue = avgValue;
+	}
+	
+	public void setMedianValue(double medValue)
+	{
+		this.medianValue = medValue;
+	}
+	
+	public void setScoreWeightValue(double scoreWgtValue)
+	{
+		this.scoreWeightValue = scoreWgtValue;
+	}
+	
+	public void setFirstQuartile(double firstQrtile)
+	{
+		this.firstQuartile = firstQrtile;
+	}
+	
+	public void setThirdQuartile(double thirdQrtile)
+	{
+		this.thirdQuartile = thirdQrtile;
+	}
+	
+	public void setBetterValue(BetterValue betterValue)
+	{
+		this.betterValue = betterValue;
 	}
 }
