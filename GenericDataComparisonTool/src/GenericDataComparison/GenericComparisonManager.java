a72407f2f193ee77579e02bc4ea6a898b4a2af77
Shaunavon Blackmore <Shaunavon Blackmore@192.168.0.12>
2018-07-25 23:24:23 -0500

    Add three new packages for back-end, UI and Unit tests codes

---
+++ GenericDataComparisonTool/src/GenericDataComparison/GenericComparisonManager.java
@@ -0,0 +1,81 @@
+package GenericDataComparison;
+import java.util.ArrayList;
+
+public class GenericComparisonManager {
+	private ObjectType objectType;
+	private UserComparisonEntry userComparisonEntry;
+	private ComparisonChart comparisonChart;
+	
+	public GenericComparisonManager() {
+		objectType = new ObjectType();
+		userComparisonEntry = new UserComparisonEntry();
+	}
+	
+	//	Get all Object Types
+	public ArrayList<ObjectType> getObjectTypes(){
+		return objectType.getAllObjectTypes();
+	}
+	
+	//	Get Object Type by name
+	public ObjectType getObjectTypeByName(String name) {
+		return objectType.getObjectTypeByName(name);
+	}
+	
+	//	Save an Object Type
+	public void saveObjectType(String name) {
+		objectType.setName(name);
+		try {
+			objectType.saveObjectTypeName();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			//	Add message of failure to save object Type
+		}
+	}
+	
+	//	Delete an Object Type
+	public void deleteObjectType(String name) {
+		objectType.deleteObjectType(name);
+	}
+	
+	//	Update an Object Type
+	
+	
+	// 	Add Characteristics to Object Type
+	public void addCharacteristics(String objectTypeName, ArrayList<Characteristic> characteristics)
+	{
+		objectType.setName(objectTypeName);
+		objectType.setCharacteristics(characteristics);
+		objectType.addCharacteristicsToObjectType();
+	}
+	
+	//	Delete Characteristic from Object Type
+	
+	//	Add User Comparison Entry to Object Type
+	
+	//	Delete User comparison entry from object Type
+	
+	//	Update User Comparison entry to Object type
+	
+	//	Generate JFreeChart
+	public void generateChart(String name, ObjectType baseLineData, UserComparisonEntry userData, Boolean createLegend) {
+		comparisonChart = new ComparisonChart(name, baseLineData, userData, createLegend);
+		comparisonChart.generateChart();
+	}
+	
+	public ObjectType getObjectType() {
+		return this.objectType;
+	}
+	
+	public UserComparisonEntry getUserComparisonEntry() {
+		return this.userComparisonEntry;
+	}
+	
+	public void setObjectType(ObjectType objectType) {
+		this.objectType = objectType;
+	}
+	
+	public void setUserComparisonEntry(UserComparisonEntry userComparisonEntry) {
+		this.userComparisonEntry = userComparisonEntry;
+	}
+}
--- GenericDataComparisonTool/src/GenericDataComparison/Main.java
+++ GenericDataComparisonTool/src/GenericDataComparison/Main.java
@@ -1,3 +1,4 @@
+package GenericDataComparison;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
