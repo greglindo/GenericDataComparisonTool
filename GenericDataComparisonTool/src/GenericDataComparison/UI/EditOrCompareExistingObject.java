1bb23f55c5d4b4fa6ff0aebeb4a27586fcad6d1b
Saba <Saba@Windows8.fios-router.home>
2018-08-01 14:13:32 -0400

    Testing commit of icons

--- GenericDataComparisonTool/src/GenericDataComparison/UI/EditOrCompareExistingObject.java
+++ GenericDataComparisonTool/src/GenericDataComparison/UI/EditOrCompareExistingObject.java
@@ -4,6 +4,10 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class EditOrCompareExistingObject {
 
@@ -42,12 +46,48 @@ public class EditOrCompareExistingObject {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(759, 456);
 		shell.setText("SWT Application");
 		
-		Label lblNewLabel = new Label(shell, SWT.NONE);
-		lblNewLabel.setBounds(10, 10, 283, 16);
-		lblNewLabel.setText("Edit Or Compare Existing Object");
+		Label lblNewLabel = new Label(shell, SWT.WRAP);
+		lblNewLabel.setAlignment(SWT.CENTER);
+		lblNewLabel.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		lblNewLabel.setBounds(10, 98, 723, 30);
+		lblNewLabel.setText("Select an object for comparison. Or, click [placeholder] to edit baseline object.");
+		
+		Button btnNewButton = new Button(shell, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(347, 382, 75, 25);
+		btnNewButton.setText("Back");
+		
+		Label label = new Label(shell, SWT.WRAP);
+		label.setText("Edit Or Compare Existing Object");
+		label.setFont(SWTResourceManager.getFont("Segoe UI", 32, SWT.NORMAL));
+		label.setAlignment(SWT.CENTER);
+		label.setBounds(0, 10, 723, 72);
+		
+		Button btnDelete = new Button(shell, SWT.NONE);
+		btnDelete.setImage(SWTResourceManager.getImage(EditOrCompareExistingObject.class, "/GenericDataComparison/UI/img/Delete.png"));
+		btnDelete.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnDelete.setText("Delete");
+		btnDelete.setBounds(347, 173, 75, 25);
+		
+		Button btnEdit = new Button(shell, SWT.NONE);
+		btnEdit.setImage(SWTResourceManager.getImage(EditOrCompareExistingObject.class, "/GenericDataComparison/UI/img/List.png"));
+		btnEdit.setText("Edit");
+		btnEdit.setBounds(253, 173, 75, 25);
+		
+		Button btnCar = new Button(shell, SWT.NONE);
+		btnCar.setText("Car");
+		btnCar.setBounds(441, 173, 75, 25);
 
 	}
 }
