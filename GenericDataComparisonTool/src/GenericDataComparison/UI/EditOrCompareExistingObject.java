ab6b3515b80776c23efb0a04f74c2d98941048eb
Greg Lindo <greglindo@comcast.net>
2018-08-02 21:09:34 -0400

    Integrate views into main

--- GenericDataComparisonTool/src/GenericDataComparison/UI/EditOrCompareExistingObject.java
+++ GenericDataComparisonTool/src/GenericDataComparison/UI/EditOrCompareExistingObject.java
@@ -1,53 +1,12 @@
 package GenericDataComparison.UI;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.SWT;
+import javax.swing.JPanel;
+import GenericDataComparison.Main;
 
-public class EditOrCompareExistingObject {
-
-	protected Shell shell;
-
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			EditOrCompareExistingObject window = new EditOrCompareExistingObject();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		Display display = Display.getDefault();
-		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
-	}
-
-	/**
-	 * Create contents of the window.
-	 */
-	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+public class EditOrCompareExistingObject extends JPanel
+{
+	public EditOrCompareExistingObject(Main mainWin)
+	{
 		
-		Label lblNewLabel = new Label(shell, SWT.NONE);
-		lblNewLabel.setBounds(10, 10, 283, 16);
-		lblNewLabel.setText("Edit Or Compare Existing Object");
-
 	}
 }
